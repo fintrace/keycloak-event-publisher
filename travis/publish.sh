37b2a58a3cd381b92f6accd172bdd7f9f4c05264
Venkaiah Chowdary Koneru <koneru.chowdary@gmail.com>
2018-03-24 12:10:17 +0400

    remove multi threads

--- travis/publish.sh
+++ travis/publish.sh
@@ -2,6 +2,6 @@
 
 if [[ -n "$TRAVIS_TAG"]]; then
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.kbx.enc -out $GPG_DIR/pubring.kbx -d
-    mvn deploy -B -U -T2 -fae --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    mvn deploy -B -U --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
     exit $?
 fi
